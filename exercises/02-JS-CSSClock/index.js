f580724995b7b40388bbabc015c2a00e02d38147
Greg Foreman <gfore@mindspring.com>
2018-12-17 21:55:14 -0500

    adds second-hand functionality

--- exercises/02-JS-CSSClock/index.js
+++ exercises/02-JS-CSSClock/index.js
@@ -1,1 +1,11 @@
-console.log("Let's do this, baby!")
+const secondHand = document.querySelector('[data-second]');
+
+function setDate() {
+    const now = new Date();
+    const secondsDegrees = (now.getSeconds()/60)*360;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    console.log(secondsDegrees)
+}
+
+setInterval(setDate, 1000)
