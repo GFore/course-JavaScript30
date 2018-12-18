4b04905bb448640b1da99d577a9c75d9301976b1
Greg Foreman <gfore@mindspring.com>
2018-12-17 22:09:57 -0500

    adds minute and hour-hand functionality

--- exercises/02-JS-CSSClock/index.js
+++ exercises/02-JS-CSSClock/index.js
@@ -1,11 +1,17 @@
 const secondHand = document.querySelector('[data-second]');
+const minuteHand = document.querySelector('[data-minute]');
+const hourHand = document.querySelector('[data-hour]');
 
 function setDate() {
     const now = new Date();
     const secondsDegrees = (now.getSeconds()/60)*360;
+    const minutesDegrees = (now.getMinutes()/60)*360;
+    const hourDegrees = (now.getHours()/12)*360;
 
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(secondsDegrees)
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    //console.log(secondsDegrees)
 }
 
 setInterval(setDate, 1000)
