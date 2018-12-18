6907cf6bcb14d5e6441e815d1271550507474492
Greg Foreman <gfore@mindspring.com>
2018-12-18 10:20:07 -0500

    fixes motion of the hour hand and hand moving from 59 to 0

--- exercises/02-JS-CSSClock/index.js
+++ exercises/02-JS-CSSClock/index.js
@@ -4,17 +4,31 @@ const hourHand = document.querySelector('[data-hour]');
 const digitalTime = document.querySelector('[data-digTime]');
 const digitalDate = document.querySelector('[data-date]');
 
-function setDate() {
+function setHands() {
     const now = new Date();
     const secondsDegrees = (now.getSeconds()/60)*360;
     const minutesDegrees = (now.getMinutes()/60)*360;
-    const hourDegrees = (now.getHours()/12)*360;
+    const hoursDegrees = (now.getHours()/12)*360 + Math.floor(now.getMinutes()/12)*6;
 
+    // Toggle off the CSS transition when the hand needs to move from
+    // the 59 sec position to the 0 sec postion to avoid the hand
+    // appearing to move counter-clockwise from 59 to 0.
+    if (secondsDegrees === 354 || secondsDegrees === 6) {
+        secondHand.classList.toggle('ticker');
+    };
+    if (minutesDegrees === 354 || minutesDegrees === 6) {
+        minuteHand.classList.toggle('ticker');
+    };
+    if (hoursDegrees === 354 || hoursDegrees === 6) {
+        hourHand.classList.toggle('ticker');
+    };
+    
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
     digitalTime.innerHTML = now.toLocaleTimeString();
     digitalDate.innerHTML = now.toLocaleDateString();
 }
 
-setInterval(setDate, 1000)
+setInterval(setHands, 1000)
